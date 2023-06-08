a409a9136b0109aafd375f33d61ca9a7f0572e3d
STAxEN23 <132597293+STAxEN23@users.noreply.github.com>
2023-06-08 17:22:49 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN_BOT = r"6051744588:AAHhEMjJipfYY3hfzzuPQ36j8puj4BrkYzk"
+API_KEY = "fbd38bdf2013b40abda7"
+list_of_currencies = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
